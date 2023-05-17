134a2608e02e31e71d4b58d130eb0278ab590315
최치훈 <chchih99@gmail.com>
2023-05-17 09:31:08 +0900

    백엔드 세팅

--- final_pjt_back/final_pjt_back/settings.py
+++ final_pjt_back/final_pjt_back/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+CORS_ALLOW_ALL_ORIGINS = True
 
 # Application definition
 
@@ -34,6 +35,19 @@ INSTALLED_APPS = [
     'accounts',
     'articles',
     'rest_framework',
+    "corsheaders",
+
+    # Auth
+    'rest_framework.authtoken',
+    'dj_rest_auth',
+
+    # registration
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'dj_rest_auth.registration',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -45,12 +59,33 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+REST_AUTH = { # 회원가입시 토큰 발급
+    'SESSION_LOGIN': False,
+}
+
+SITE_ID = 1
+# 하나의 컨텐츠로 여러 개의 도메인에 등록하고 싶을 때 사용
+
+REST_FRAMEWORK = {
+    # Authentication
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+
+    # permission
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAuthenticated', # 디폴트로 다 막기
+        'rest_framework.permissions.AllowAny',      # 디폴트로 다 허용
+    ],
+}
 
 ROOT_URLCONF = 'final_pjt_back.urls'
 
@@ -126,3 +161,13 @@ STATIC_URL = '/static/'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'accounts.User'
+
+# dj_rest_auth의 설정
+REST_AUTH = {
+    'REGISTER_SERIALIZER': 'accounts.serializers.RegisterSerializer'
+}
+
+# allauth의 default adapter 설정
+# ACCOUNT_ADAPTER = 'accounts.models.CustomAccountAdapter'
