6783aba64a4b542250e3a45475a2baf1a3d1ae8a
최치훈 <chchih99@gmail.com>
2023-05-22 16:34:09 +0900

    new css meta

--- final_pjt_front/src/components/recommend/ChoiceCard.vue
+++ final_pjt_front/src/components/recommend/ChoiceCard.vue
@@ -1,5 +1,5 @@
 <template>
-    <div>
+    <div class="outer">
         
     </div>
 </template>
@@ -24,6 +24,36 @@ export default {
 };
 </script>
 
-<style lang="scss" scoped>
+<style scoped>
+.outer {
+    background-color: lightsalmon;
+    width: 300px;
+    height: 500px;
+    border-radius: 20px;
+    box-shadow: 0px 8px 20px rgba(0, 0, 0, 0.4);
+    transition: transform 0.3s, box-shadow 0.3s;
+    margin: 50px;
+}
+
+.outer:hover {
+    transform: scale(1.05);
+    box-shadow: 0px 12px 40px rgba(0, 0, 0, 0.6);
+}
+
+@keyframes pulse {
+    0% {
+        transform: scale(1);
+    }
+    50% {
+        transform: scale(1.1);
+    }
+    100% {
+        transform: scale(1);
+    }
+}
+
+.outer:hover {
+    animation: pulse 0.6s infinite;
+}
 
 </style>
