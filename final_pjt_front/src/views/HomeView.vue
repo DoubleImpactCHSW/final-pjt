0f2fafc9cccb9e4909ce15e41ff7a9ff92066a53
최치훈 <chchih99@gmail.com>
2023-05-25 17:57:44 +0900

    bigger font

--- final_pjt_front/src/views/HomeView.vue
+++ final_pjt_front/src/views/HomeView.vue
@@ -15,8 +15,8 @@
         <div @click="goToExchangeView">
           <b-card overlay :img-src="getImagePath('1.jpg')" img-alt="Card Image" text-variant="white" sub-title="">
             <div class="d-flex flex-column justify-content-center align-items-end h-100">
-              <b-card-title class="text-right"><strong style="font-size: 40px; margin-right: 30px;">국가별 환율을 한눈에</strong></b-card-title>
-              <b-card-text class="text-right" style="font-size: 20px; margin-right: 30px;">
+              <b-card-title class="text-right"><strong style="font-size: 80px; margin-right: 30px;">국가별 환율을 한눈에</strong></b-card-title>
+              <b-card-text class="text-right" style="font-size: 40px; margin-right: 30px;">
                 지금 환율이 가장 좋은 곳은?
                 환율 조회해보자~
               </b-card-text>
@@ -30,8 +30,8 @@
         <div @click="goToProductView">
           <b-card overlay :img-src="getImagePath('2.jpg')" img-alt="Card Image" text-variant="white" sub-title="" >
           <div class = "d-flex flex-column justify-content-center align-items-start h-100">
-              <b-card-title class="text-left" style="font-size: 40px; margin-right: 40px;"><strong>예금금리비교</strong></b-card-title>
-              <b-card-text class="text-left" style="font-size: 20px; margin-right: 40px;">전국 1만여개 금융기관을 한번에~</b-card-text>
+              <b-card-title class="text-left" style="font-size: 80px; margin-right: 40px;"><strong>예금금리비교</strong></b-card-title>
+              <b-card-text class="text-left" style="font-size: 40px; margin-right: 40px;">전국 1만여개 금융기관을 한번에~</b-card-text>
         </div>
           </b-card>
         </div>
@@ -42,8 +42,8 @@
         <div @click="goToBankMapView">
           <b-card overlay :img-src="getImagePath('3.jpg')" img-alt="Card Image" text-variant="white" sub-title="" >
           <div class = "d-flex flex-column justify-content-center align-items-end h-100">
-              <b-card-title class="text-right "><strong style="font-size: 40px; margin-right: 30px;">은행 찾기</strong></b-card-title>
-              <b-card-text class="text-right" style="font-size: 20px; margin-right: 30px;">이 근처에 은행이 어디 있을까? 확인하고 싶다면?!</b-card-text>
+              <b-card-title class="text-right "><strong style="font-size: 80px; margin-right: 30px;">은행 찾기</strong></b-card-title>
+              <b-card-text class="text-right" style="font-size: 40px; margin-right: 30px;">이 근처에 은행이 어디 있을까? 확인하고 싶다면?!</b-card-text>
           </div>
           </b-card>
         </div>
@@ -54,8 +54,8 @@
         <div @click="goToRecommendView">
           <b-card overlay :img-src="getImagePath('4.jpg')" img-alt="Card Image" text-variant="white" >
           <div class = "d-flex flex-column justify-content-center align-items-end h-100">
-              <b-card-title class="text-right"><strong style="font-size: 40px; margin-right: 30px;">내게 맞는 예적금 상품</strong></b-card-title>
-              <b-card-text class="text-right" style="font-size: 20px; margin-right: 30px;">EZ는 쉽고 정확하게 추천해드립니다.</b-card-text>
+              <b-card-title class="text-right"><strong style="font-size: 80px; margin-right: 30px;">내게 맞는 예적금 상품</strong></b-card-title>
+              <b-card-text class="text-right" style="font-size: 40px; margin-right: 30px;">EZ는 쉽고 정확하게 추천해드립니다.</b-card-text>
           </div>
           </b-card>
         </div>
